121a7bf36c518a1af01b5a4a22b36ae1f2d186b9
takeshibruno <102680186+takeshibruno@users.noreply.github.com>
2024-09-26 18:28:06 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,16 @@
+package pratica03.estruturas_de_repeticao1ao15;
+import javax.swing.JOptionPane;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            JOptionPane.showMessageDialog(null, "Número: " + i);
+        }
+    }
+}
+
+
+
+
+
+
