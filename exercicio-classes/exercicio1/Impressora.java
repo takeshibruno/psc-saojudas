6e98b33cfd76ec86820ad135382aa2b26d486674
Bruno <bruno@rodpheus.net>
2024-10-03 20:17:17 -0300

    finalizado

--- exercicio-classes/exercicio1/Impressora.java
+++ exercicio-classes/exercicio1/Impressora.java
@@ -1,5 +1,30 @@
 package exercicio1;
 
 public class Impressora { 
-    
+    static void exibir (float numero){
+        System.out.println(numero);
+    }
+
+    static void exibir (float numero1, float numero2){
+        //System.out.printf("%d, %d", numero1, numero2);
+        System.out.println(numero1 + ", " + numero2);
+    }
+
+    static void exibir (float numero, String input){
+        //System.out.printf("%d, %s", numero, input);
+        System.out.println(numero + ", " + input);
+    }
+
+    static void exibir (String input, float numero){
+        //System.out.printf("%s, %d", input, numero);
+        System.out.println(input + ", " + numero);
+    }
+
+    static void exibir (String input1, String input2, String input3){
+        //System.out.printf("%s, %s, %s", input1, input2, input3);
+    }
+
+    static void exibir (int numero1, int numero2, String input){
+        //System.out.printf("%d, %d, %s", numero1, numero2, input);
+    }
 }
--- exercicio-classes/exercicio1/execute.java
+++ exercicio-classes/exercicio1/execute.java
@@ -2,6 +2,6 @@ package exercicio1;
 
 public class execute {
     public static void main(String[] args) {
-        
+        Impressora.exibir(2, 4, "Bruno");
     }
 }
